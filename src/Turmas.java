6541777c47dea4a40b4cc3a9dd24d7d36b9b73a1
Lucas Ferreira <lucas.santana220206@gmail.com>
2025-05-12 18:00:21 -0300

    Teste 2

--- src/Turmas.java
+++ src/Turmas.java
@@ -17,7 +17,5 @@ public class Turmas {
     public String getCodigo() {return codigo; }
     public String getProfessor() {return professor; }
     public String getHorario() {return horario; }
-    public String getSala() {return sala; } //acessar o atributo sala
-
-
+    public String getSala() {return sala; }
 }
