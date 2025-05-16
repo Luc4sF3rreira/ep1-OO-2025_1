44b7edf5787140c063b9e0912c093c93ad41051e
Lucas Ferreira <lucas.santana220206@gmail.com>
2025-05-16 19:43:29 -0300

    Correções e aperfeiçoamentos

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -1,6 +1,4 @@
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Disciplina {
     protected String nome;
--- src/Turmas.java
+++ src/Turmas.java
@@ -1,6 +1,4 @@
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Turmas extends Disciplina {
     private String professor;
