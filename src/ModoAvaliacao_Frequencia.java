d8bb3c92696e4d51c7015dc93349b9dbe341e816
Lucas Ferreira <lucas.santana220206@gmail.com>
2025-05-17 16:42:17 -0300

    Últimos ajustes das classes, falta somente criar a main principal e realizar os ajustes necessários

--- src/ModoAvaliacao_Frequencia.java
+++ src/ModoAvaliacao_Frequencia.java
@@ -1,54 +1,106 @@
-public class ModoAvaliacao_Frequencia {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-    private String turma;
-    private double[] notas;
-    private int[] presencas;
+public class ModoAvaliacao_Frequencia {  
+    public void lancarNotasParaTurma(Turmas turma) {
+        Scanner scanner = new Scanner(System.in);    
+            for (Aluno aluno : turma.getAlunosMatriculados()) {
+                System.out.println("Lançando notas para o aluno: " + aluno.getNome());
+    
+                System.out.print("Prova 1: ");
+                double prova1 = scanner.nextDouble();
+    
+                System.out.print("Prova 2: ");
+                double prova2 = scanner.nextDouble();
+    
+                System.out.print("Prova 3: ");
+                double prova3 = scanner.nextDouble();
+    
+                System.out.print("Listas de Exercícios: ");
+                double listasExercicios = scanner.nextDouble();
+    
+                System.out.print("Seminário: ");
+                double trabalhos = scanner.nextDouble();
 
-    public ModoAvaliacao_Frequencia(String turma, int numeroDeAlunos) {
-        this.turma = turma;
-        this.notas = new double[numeroDeAlunos];
-        this.presencas = new int[numeroDeAlunos];
-    }
+                System.out.print("Aulas presentes: ");
+                int presenca = scanner.nextInt();
+    
+                Avaliacao avaliacao = new Avaliacao(turma, prova1, prova2, prova3, listasExercicios, trabalhos, presenca, turma.getTotalAulas());               
+                aluno.setAvaliacao(avaliacao);
 
-    public void lancarNota(int alunoIndex, double nota) {
-        if (alunoIndex >= 0 && alunoIndex < notas.length) {
-            notas[alunoIndex] = nota;
-        } else {
-            System.out.println("Índice de aluno inválido.");
+                double mediaFinal = avaliacao.calcularMediaFinal();
+                System.out.println("Média final do aluno " + aluno.getNome() + ": " + mediaFinal);                
+
+                double frequencia = avaliacao.calcularFrequencia();
+                System.out.println("A frequência do aluno foi " + aluno.getNome() + ": " + frequencia);
+                System.out.println("---------------------------------------------");
+
+                String situacao = avaliacao.consultarSituacao();
+                System.out.println("O aluno foi: " + situacao);
         }
     }
 
-    public void lancarPresenca(int alunoIndex, int presenca) {
-        if (alunoIndex >= 0 && alunoIndex < presencas.length) {
-            presencas[alunoIndex] = presenca;
-        } else {
-            System.out.println("Índice de aluno inválido.");
+    public void relatorioPorTurma(Turmas turma) {
+        System.out.println("Relatório da Turma: " + turma.getNome());
+        for (Aluno aluno : turma.getAlunosMatriculados()) {
+            Avaliacao avaliacao = aluno.getAvaliacao();
+            if (avaliacao != null) {
+                System.out.println("Aluno: " + aluno.getNome());
+                System.out.println("Média Final: " + avaliacao.calcularMediaFinal());
+                System.out.println("Frequência: " + avaliacao.calcularFrequencia());
+                System.out.println("Situação: " + avaliacao.consultarSituacao());
+                System.out.println("---------------------------------------------");
+            } else {
+                System.out.println("Aluno: " + aluno.getNome() + " - Avaliação não lançada.");
+            }
         }
     }
 
-    public double consultarNota(int alunoIndex) {
-        if (alunoIndex >= 0 && alunoIndex < notas.length) {
-            return notas[alunoIndex];
-        } else {
-            System.out.println("Índice de aluno inválido.");
-            return -1;
+    public void relatorioPorDisciplina(List<Turmas> turmas, String nome) {
+        System.out.println("Relatório da Disciplina: " + nome);
+        for (Turmas turma : turmas) {
+            if (turma.getNome().equalsIgnoreCase(nome)) {
+                relatorioPorTurma(turma);
+            }
         }
     }
 
-    public int consultarPresenca(int alunoIndex) {
-        if (alunoIndex >= 0 && alunoIndex < presencas.length) {
-            return presencas[alunoIndex];
-        } else {
-            System.out.println("Índice de aluno inválido.");
-            return -1;
+    public void relatorioPorProfessor(List<Turmas> turmas, String professor) {
+        System.out.println("Relatório do Professor: " + professor);
+        for (Turmas turma : turmas) {
+            if (turma.getProfessor().equalsIgnoreCase(professor)) {
+                relatorioPorTurma(turma);
+            }
         }
     }
-
-    public String getTurma() {
-        return turma;
-    }
-
-    public void setTurma(String turma) {
-        this.turma = turma;
+    
+    public void exibirBoletimPorAluno(List<Turmas> turmas, String nomeAluno, boolean exibirDadosTurma) {
+        System.out.println("Boletim do Aluno: " + nomeAluno);
+        for (Turmas turma : turmas) {
+            for (Aluno aluno : turma.getAlunosMatriculados()) {
+                if (aluno.getNome().equalsIgnoreCase(nomeAluno)) {
+                    Avaliacao avaliacao = aluno.getAvaliacao();
+                    if (avaliacao != null) {
+                        System.out.println("Semestre: " + turma.getSemestre());
+                        if (exibirDadosTurma) {
+                            System.out.println("Disciplina: " + turma.getNome());
+                            System.out.println("Professor: " + turma.getProfessor());
+                            System.out.println("Modalidade: " + (turma.isPresencial() ? "Presencial" : "Remota"));
+                            System.out.println("Carga Horária: " + turma.getCargaHoraria() + " horas");
+                        }
+                        System.out.println("Média Final: " + avaliacao.calcularMediaFinal());
+                        System.out.println("Frequência: " + avaliacao.calcularFrequencia());
+                        System.out.println("Situação: " + avaliacao.consultarSituacao());
+                        System.out.println("---------------------------------------------");
+                    } else {
+                        System.out.println("Semestre: " + turma.getSemestre());
+                        System.out.println("Disciplina: " + turma.getNome());
+                        System.out.println("Avaliação não lançada.");
+                        System.out.println("---------------------------------------------");
+                    }
+                }
+            }
+        }
     }
 }
